53c3aaf06a5cb3dbb1687db4f6a9940cd9315cc1
pesrisaw <peerapol1611@gmail.com>
2025-02-11 17:56:46 +0700

    npm run build

--- Frontend/cepp-app/app/dnd/page.tsx
+++ Frontend/cepp-app/app/dnd/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
 import Board from "./_components/Board";
-// import { observer } from "./Game";
 const DndPage = () => {
 
     return (
