8027c0fb5f93e45188ee3d5489032b3889b5c8b6
pesrisaw <peerapol1611@gmail.com>
2025-02-11 17:46:00 +0700

    change position hourse

--- Frontend/cepp-app/app/dnd/page.tsx
+++ Frontend/cepp-app/app/dnd/page.tsx
@@ -7,7 +7,7 @@ const DndPage = () => {
 
     return (
         <div className="h-screen w-screen flex justify-center items-center">
-            <Board knightPosition={[7, 4]}/>
+            <Board knightPosition={[1, 4]}/>
         </div>
     )
 }
